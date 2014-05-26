48c2e2ad76039fb69bae75816d0685daaa9cc5e5
William Studer <bill.studer@itconcepts-inc.com>
2014-05-25 23:50:44 -0400

    adding workflow build test

---
+++ src/test/java/com/studerw/design/SimpleWorkflowTest.java
@@ -0,0 +1,118 @@
+package com.studerw.design;
+
+import org.activiti.bpmn.BpmnAutoLayout;
+import org.activiti.bpmn.model.*;
+import org.activiti.bpmn.model.Process;
+import org.activiti.engine.*;
+import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.*;
+import org.activiti.engine.task.Task;
+import org.apache.commons.io.FileUtils;
+import org.apache.log4j.Logger;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.List;
+
+/**
+ * User: studerw
+ * Date: 5/25/14
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring/testAppContext.xml"})
+public class SimpleWorkflowTest {
+    private static final Logger log = Logger.getLogger(SimpleWorkflowTest.class);
+    @Autowired
+    RuntimeService runtimeService;
+    @Autowired
+    TaskService taskService;
+    @Autowired
+    HistoryService historyService;
+    @Autowired
+    IdentityService identityService;
+    @Autowired
+    RepositoryService repositoryService;
+
+    @Test
+    public void testDynamicDeploy() throws Exception {
+        // 1. Build up the model from scratch
+        BpmnModel model = new BpmnModel();
+        Process process = new Process();
+        model.addProcess(process);
+        process.setId("my-process");
+
+        process.addFlowElement(createStartEvent());
+        process.addFlowElement(createUserTask("task1", "First task", "fred"));
+        process.addFlowElement(createUserTask("task2", "Second task", "john"));
+        process.addFlowElement(createEndEvent());
+
+        process.addFlowElement(createSequenceFlow("start", "task1"));
+        process.addFlowElement(createSequenceFlow("task1", "task2"));
+        process.addFlowElement(createSequenceFlow("task2", "end"));
+
+        // 2. Generate graphical information
+        new BpmnAutoLayout(model).execute();
+
+        // 3. Deploy the process to the engine
+        Deployment deployment = this.repositoryService.createDeployment()
+                .addBpmnModel("dynamic-model.bpmn", model).name("Dynamic process deployment")
+                .deploy();
+
+        // 4. Start a process instance
+        ProcessInstance processInstance = runtimeService
+                .startProcessInstanceByKey("my-process");
+
+        // 5. Check if task is available
+        List<Task> tasks = taskService.createTaskQuery()
+                .processInstanceId(processInstance.getId()).list();
+
+        assertEquals(1, tasks.size());
+        assertEquals("First task", tasks.get(0).getName());
+        assertEquals("fred", tasks.get(0).getAssignee());
+
+        // 6. Save process diagram to a file
+        InputStream processDiagram = repositoryService
+                .getProcessDiagram(processInstance.getProcessDefinitionId());
+        FileUtils.copyInputStreamToFile(processDiagram, new File("target/diagram.png"));
+
+        // 7. Save resulting BPMN xml to a file
+        InputStream processBpmn = repositoryService
+                .getResourceAsStream(deployment.getId(), "dynamic-model.bpmn");
+        FileUtils.copyInputStreamToFile(processBpmn,
+                new File("target/process.bpmn20.xml"));
+    }
+
+    protected UserTask createUserTask(String id, String name, String assignee) {
+        UserTask userTask = new UserTask();
+        userTask.setName(name);
+        userTask.setId(id);
+        userTask.setAssignee(assignee);
+        return userTask;
+    }
+
+    protected SequenceFlow createSequenceFlow(String from, String to) {
+        SequenceFlow flow = new SequenceFlow();
+        flow.setSourceRef(from);
+        flow.setTargetRef(to);
+        return flow;
+    }
+
+    protected StartEvent createStartEvent() {
+        StartEvent startEvent = new StartEvent();
+        startEvent.setId("start");
+        return startEvent;
+    }
+
+    protected EndEvent createEndEvent() {
+        EndEvent endEvent = new EndEvent();
+        endEvent.setId("end");
+        return endEvent;
+    }
+}
